465fba3c0db4eed1f75bef9dd5cdc25df8bb19d1
gitkapsource <40160096+gitkapsource@users.noreply.github.com>
2026-02-05 13:47:16 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -60,7 +60,7 @@ const Footer = () => {
             <div className="space-y-3">
               <div className="flex items-center space-x-3">
                 <Phone className="h-4 w-4 text-blue-400" />
-                <span className="text-gray-300">+91-7935901473</span>
+                <span className="text-gray-300">+16824280781</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="h-4 w-4 text-blue-400" />
